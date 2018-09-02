0ca3a694633ea7027bd19a1bef175dd9bc557cef
Alex Chi <iSkyZH@gmail.com>
2018-09-02 16:56:00 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     <ResourceViewer class="resource" :source="'Raspi_Mon'" :subscription="'Memory'" :fill="'#64d8cb'" :stroke="'#26a69a'" :precision="source" :from="from" :limit="120" :live="true"></ResourceViewer>
     <ResourceViewer class="resource" :source="'Raspi_Mon'" :title="'Temp.'" :subscription="'Temperature'" :fill="'#ffff89'" :stroke="'#d4e157'" :precision="source" :from="from" :limit="120" :live="true"></ResourceViewer>
     <ResourceViewer class="resource" :source="'Raspi_Mon'" :subscription="'Voltage'" :fill="'#ffd95b'" :stroke="'#ffa726'" :precision="source" :from="from" :limit="120" :live="true"></ResourceViewer>
-    <p>An <a href="https://github.com/SkyZH/Skynetcomm">open-source project</a> by <a href="https://github.com/SkyZH">Sky Zhang</a></p>
+    <p>An <a href="https://github.com/SkyZH/Skynetcomm">open-source project</a> by <a href="https://github.com/SkyZH">Alex Chi</a></p>
     <p>Ver. {{ VERSION }}</p>
     <img width="24" height="24" src="/logo.png">
   </div>
